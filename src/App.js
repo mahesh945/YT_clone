04c7ef3c150de1bbe2bdc88e982d4abe950b72cb
mahesh945 <mp.mahesh2200@gmail.com>
2023-02-17 20:42:28 +0530

    New categories added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Feed from './components/Feed';
 import VideoDetail from './components/VideoDetail';
 import ChannelDetail from './components/ChannelDetail';
 import SearchFeed from './components/SearchFeed';
+// import SideBar from './components/SideBar';
 
 
 const App = () => {
@@ -18,7 +19,7 @@ const App = () => {
                 <Route path="/videos/:id" element={<VideoDetail />} />
                 <Route path="/channel/:id" element={<ChannelDetail />} />
                 <Route path="/search/:searchTerm" element={<SearchFeed />} />
-                console.log("this is appjs");
+               
 
             </Routes>
         </Box>
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,8 @@
 import { Typography } from '@mui/material'
 import { Box, Stack } from '@mui/system'
 import { useState, useEffect } from 'react'
+import SideBar from './SideBar'
+import Videos from './Videos'
 
 const Feed = () => {
   return (
@@ -13,13 +15,20 @@ const Feed = () => {
       <Box sx={{
         height: { sx: 'auto', md: '92vh' },
         borderRight: '1px solid #3d3d3d',
-        px: { sx: 0, md: 2 }
-      }}>
+        px: { sx: 0, md: 2 }}}>
+          <SideBar />
         <Typography className='copyright' variant='body2' sx={{mt:1.5, color:'#fff'}}>
           Copyright @2023
         </Typography>
       </Box>
 
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+          {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
+        </Typography>
+
+        <Videos videos={videos} />
+      </Box>
     </Stack>
   )
 }
