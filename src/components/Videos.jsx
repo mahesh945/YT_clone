ba1aa895df8ed906518585691c4a846d4114b722
mahesh945 <mp.mahesh2200@gmail.com>
2023-02-27 22:39:48 +0530

    debugging

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -6,14 +6,16 @@ import Loader from './Loader';
 
 
 const Videos = ({ videos, direction }) => {
-  if(!videos?.length) return <Loader />;
-  
+  if (!videos?.length) return <Loader />;
+
   return (
     <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
       {videos.map((item, idx) => (
         <Box key={idx}>
-          {item.id.videoId && <VideoCard video={item} /> }
-          {item.id.channelId && <ChannelCard channelDetail={item} />}
+          {item.id && item.id.videoID && <VideoCard video={item} />}
+          {item.id && item.id.channelID && <ChannelCard channelDetail={item} />}
+          {/* {item.id.videoId && <VideoCard video={item} /> }
+          {item.id.channelId && <ChannelCard channelDetail={item} />} */}
         </Box>
       ))}
     </Stack>
--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,17 +1,17 @@
 import axios from 'axios';
 
-export const BASE_URL='https://youtube-v31.p.rapidapi.com/';
+export const BASE_URL='https://youtube-v3-alternative.p.rapidapi.com';
 
 const options = {
     
-    params: {part: 'snippet', videoId: 'M7FIvfx5J10'},
+    params: {maxResults: '50'},
     headers: {
-      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-      'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+      'X-RapidAPI-Key': '61ea1fd126msh6612cb1a80ca52bp14798ajsnef72fe167ede',
+      'X-RapidAPI-Host': 'youtube-v3-alternative.p.rapidapi.com'
     }
   };
 
 export const fetchFromAPI = async(url) => {
-   const {data} = await axios.post(`${BASE_URL}/${url}`, options);
+   const {data} = await axios.get(`${BASE_URL}/${url}`, options);
    return data;
 }
