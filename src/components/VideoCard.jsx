e828828a33bee9049d582348b735903dbe291b5a
mahesh945 <mp.mahesh2200@gmail.com>
2023-05-03 11:35:12 +0530

    tweaks

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -6,6 +6,7 @@ import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from "../utils/constants";
 
 const VideoCard = ({ video: { id: { videoId }, snippet } }) => (
+  
   <Card sx={{ width: { xs: '100%', sm: '358px', md: "320px", }, boxShadow: "none", borderRadius: 0 }}>
     <Link to={videoId ? `/video/${videoId}` : `/video/cV2gBU6hKfY` }>
       <CardMedia image={snippet?.thumbnails?.high?.url || demoThumbnailUrl} alt={snippet?.title} 
@@ -26,6 +27,7 @@ const VideoCard = ({ video: { id: { videoId }, snippet } }) => (
       </Link>
     </CardContent>
   </Card>
+  
 );
 
 export default VideoCard
